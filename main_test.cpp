425a4ed55d7f3f1789f2264fdaacced4eb2b22c5
NicoBeade <75858058+NicoBeade@users.noreply.github.com>
2023-05-18 16:49:13 -0300

    test parser

--- main_test.cpp
+++ main_test.cpp
@@ -9,6 +9,7 @@
  */
 
 #include <iostream>
+#include <string>
 
 #include <microhttpd.h>
 
@@ -18,12 +19,58 @@
 
 using namespace std;
 
+string parseHTML(const std::string& html) {
+    string parsedHTML;
+    bool insideTag = false;
+    bool ignoreContent = false;
+
+    for (size_t i = 0; i < html.length(); ++i) {
+        if (!ignoreContent) {
+            if (html[i] == '<' && html[i + 1] == '\\') {
+                insideTag = false;
+                ignoreContent = true;
+                ++i;
+            }
+            else if (html[i] == '<') {
+                insideTag = true;
+                continue;
+            }
+            else if (html[i] == '>') {
+                insideTag = false;
+                continue;
+            }
+        }
+        else {
+            if (html[i] == '>') {
+                ignoreContent = false;
+            }
+            continue;
+        }
+
+        if (!insideTag && !ignoreContent) {
+            parsedHTML += html[i];
+        }
+    }
+    return parsedHTML;
+}
+
 void print(string s);
 int fail();
 int pass();
 
 int main()
 {
+    string prueba = "<html>\n"
+                       "<head>\n"
+                       "<title>Sample Page</title>\n"
+                       "</head>\n"
+                       "<body>\n"
+                       "<h1>Hello, World!</h1>\n"
+                       "<p>This is a sample paragraph.</p>\n"
+                       "This <\\ is not a tag.\n"
+                       "</body>\n"
+                       "</html>";
+    cout << parseHTML(prueba) << endl;
     return 0;
 }
 
