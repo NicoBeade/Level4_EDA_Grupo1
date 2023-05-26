bc1f045d5b6a2d4c1f7d0e07d354aec27f631ee0
Vito Pensa Piccolo <vitopensapiccolo@gmail.com>
2023-05-25 22:50:19 -0300

    Test function for testTermFreqCallback was created

--- main_test.cpp
+++ main_test.cpp
@@ -21,11 +21,42 @@ void print(string s);
 int fail();
 int pass();
 
+void testTermFreqCallback()
+{
+    // Create test data
+    vector<pair<string, float>> termFrequencies;
+    termFrequencies.emplace_back("path1", 0.5f);
+    termFrequencies.emplace_back("path2", 0.25f);
+    termFrequencies.emplace_back("path3", 0.75f);
 
+    // Create test arguments
+    const int argc = 2;
+    char *argv[argc] = { "path2", "0.1" };
+    char *columnNames[argc] = { "Path", "Frequency" };
+
+    // Call the termFreqCallback
+    int result = termFreqCallback(&termFrequencies, argc, argv, columnNames);
+
+    // Print the updated term frequencies
+    for (const auto &pair : termFrequencies)
+    {
+        cout << "Path: " << pair.first << ", Frequency: " << pair.second << endl;
+    }
+
+    // Print the result
+    if (result == 0)
+    {
+        pass();
+    }
+    else
+    {
+        fail();
+    }
+}
 
 int main()
 {
-
+    testTermFreqCallback();
     return 0;
 }
 
